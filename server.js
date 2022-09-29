5fc9694d8bc50b4f3627d2f7fd6ffd916d205d68
Siwarnafti <76211002+siwarnafti@users.noreply.github.com>
2022-09-29 11:24:28 +0100

    inital commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import fs from 'fs'
+import express from 'express'
+import { endianness } from 'os'
+
+const app = express()
+const hostname ='127.0.0.1'
+const port = process.env.port || 9090
+  
+class Game{
+    Constructor(name,year,url) {
+        this.name = this.name;
+        this.year = this.year;
+        this.url = this.url;
+    }
+}
+app.get('/game',(req.resp) = {
+//     readFile('SteamGames.json', (err,data) => {
+//         if(err){
+//             return res.status(404).Constructor
+//             endianness('le fichier est introuvable')
+//         }
+//         var l = JSON.parse(data);
+
+//         var games =[];
+//         for(var i =0 ; i++)
+//         var game= new Game(l[i].Game,1[i].Year,l[i].GameLink);
+//         games.push(game);
+//     })
+//   resp
+// })  
+})  
